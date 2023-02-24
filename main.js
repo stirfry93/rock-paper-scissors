d4d47988a7c8876cdcbefef9df615889f57b010f
Sterling <sterlingmrichards@gmail.com>
2023-02-24 09:59:54 -0600

    Fix score issue

--- main.js
+++ main.js
@@ -18,30 +18,30 @@ function game() {
             return "It's a tie! You both chose rock."
         }
         else if (playerSelection === 'rock' && computerSelection === 'paper') {
-            computerScore += 1;
+            computerScore++;
             return "You lose, sadly. Paper beats rock."
         }
         else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-            playerScore += 1;
+            playerScore++;
             return "You win! Rock beats scissors."
         }
         else if (playerSelection === 'paper' && computerSelection === 'rock') {
-            playerScore += 1;
+            playerScore++;
             return "You win! Paper beats rock."
         }
         else if (playerSelection === 'paper' && computerSelection === 'paper') {
             return "It's a tie! You both chose paper."
         }
         else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            computerScore += 1;
+            computerScore++;
             return "You lose, sadly. Scissors beats paper."
         }
         else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerScore += 1;
+            computerScore++;
             return "You lose, sadly. Rock beats scissors"
         }
         else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-            playerScore += 1;
+            playerScore++;
             return "You win! Scissors beats paper."
         }
         else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
@@ -55,34 +55,15 @@ function game() {
 
         const computerSelection = getComputerChoice();
 
-        playRound(playerSelection, computerSelection);
-
-        console.log(computerSelection);
         console.log(playRound(playerSelection, computerSelection));
 
+        console.log(computerSelection);
+    
         console.log(playerScore);
         console.log(computerScore);
     }
 }
 
 game();
-
-
-// after each round, award a point to the winner
-// then at the end of 5 rounds declare a winner based on who has more points, the user or the computer
-
-//let playerScore = 0
-//let computerScore = 0
-
-// if playRound == "You win! Rock beats scissors." || "You win! Paper beats rock." || "You win! Scissors beats paper." then playerScore ++
-
-// if playRound == "You lose, sadly. Paper beats rock." || "You lose, sadly. Scissors beats paper." || "You lose, sadly. Rocks beats scissors" then computerScore ++ 
-
-// for loop to play 5 rounds
-
-// at end of loop, if playerScore > computerScore declare "You won the game!"
-// else declare "You lost the game this time."
-
-
 
 
